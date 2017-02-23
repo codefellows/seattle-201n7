f051c1ff3dc273fb7f3c7838760b76a359ee2a52
Scott Schmidt <sjschmidt44@gmail.com>
2017-02-22 21:25:44 -0800

    update lecture code for class11

---
+++ class-11-av--practical--clicktracker/lecture/app.js
@@ -0,0 +1,25 @@
+// 1. Generate three random, non-dupe images (part of the controller)
+// 2. Object constructor for Products:
+  // a. Include name, path, votes
+// 3. A tracker object that will controll functionality of app
+// 4. Event listener(s) for image clicks
+
+var productImageNames = ['bag', 'banana', '...'];
+var productsArray = [];
+
+function Products() {
+
+}
+
+// a simple IIFE to build all the product images
+(function() {
+  // build the objects
+})()
+
+var tracker = {
+  // lots of properties and methods
+}
+
+someEl.addEventListener('click', function(e) {
+  // does some stuff on click
+})
