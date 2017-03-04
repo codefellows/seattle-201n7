dad3d1b593142a1a33a90ec502e51b34d88ee76a
Scott Schmidt <sjschmidt44@gmail.com>
2017-03-04 09:01:00 -0800

    upload class13 lecture code

---
+++ class-13-local_storage--ui_ux/lecture/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var ctx = document.getElementById('myChart').getContext('2d');
+
+var chartData = {
+  type: 'bar',
+  data: {
+    labels: ['hello','boots'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  }
+};
+
+// myChart.data.datasets[0].data => will get you the data array
+var myChart = new Chart(ctx, chartData);
